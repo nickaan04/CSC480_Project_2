6f75344d088b0342b6a0fbbcafdc6ccc5916a635
nickaan04 <nickaan04@gmail.com>
2025-05-11 05:15:01 -0700

    all hand evaluation

--- deck.py
+++ deck.py
@@ -59,3 +59,59 @@ def evalHand(cards: List[int]): #evaluate 5-card hand
             counts.append((count, rank))
     counts = sorted(counts, reverse=True) #sort by descending count and rank
 
+    #maybe edit logic above for royal flush
+
+    if (straight and flush):
+        if (straightHigh == 12):
+            category = 9 #royal flush
+        else:
+            category = 8 #straight flush
+            kickers = [straightHigh] + [0] * 4
+    elif (counts[0][0] == 4):
+        category = 7 #four of a kind
+        fourRank = counts[0][1]
+        kicker = ranks.index(1) #store rank of remaining card that's not part of 4 of a kind
+        kickers = [fourRank, kicker] + [0] * 3
+    elif (counts[0][0] == 3 and counts[1][0] == 2):
+        category = 6 #full house
+        threeRank = counts[0][1]
+        twoRank = counts[1][1]
+        kickers = [threeRank, twoRank] + [0] * 3
+    elif (flush):
+        category = 5 #flush
+        flushRanks = list()
+        for card in cards: #get all ranks (not necessarily their counts)
+            flushRanks.append(card % 13)
+        kickers = sorted(flushRanks, reverse=True) #sort ranks in descending order
+    elif (straight):
+        category = 4 #straight
+        kickers = [straightHigh] + [0] * 4
+    elif (counts[0][0] == 3):
+        category = 3 #three of a kind
+        threeRank = counts[0][1]
+        remainingRanks = list()
+        for card in cards: #get remaining ranks that aren't part of three of a kind
+            if (card % 13 != threeRank):
+                remainingRanks.append(card % 13)
+        kickers = [threeRank] + sorted(remainingRanks, reverse=True) + [0] * 2
+    elif (counts[0][0] == 2 and counts[1][0] == 2):
+        category = 2 #two pair
+        highPairRank, lowPairRank = counts[0][1], counts[1][1]
+        kicker = ranks.index(1) #store rank of remaining card that's not in a pair
+        kickers = [highPairRank, lowPairRank, kicker] + [0] * 2
+    elif (counts[0][0] == 2):
+        category = 1 #one pair
+        pairRank = counts[0][1]
+        remainingRanks = list()
+        for card in cards: #get remaining ranks that aren't part of the pair
+            if (card % 13 != pairRank):
+                remainingRanks.append(card % 13)
+        kickers = [pairRank] + sorted(remainingRanks, reverse=True) + [0]
+    else:
+        category = 0 #high card
+        allRanks = list()
+        for rank in range(ranks):
+            if (ranks[rank] == 1):
+                allRanks.append(rank)
+        kicker = sorted(allRanks, reverse=True)
+    
